da63859e2b7aa310c832bd2e663f01f4d85110e5
shift-d <shiftkeydeveloper@gmail.com>
2024-12-01 23:09:39 +0300

    1st day completed

---
+++ task-1.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+fn main() {
+    let file = File::open("input-1.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut l1: [u32; 1000] = [0; 1000];
+    let mut l2: [u32; 1000] = [0; 1000];
+    let mut idx = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let numerals: Vec<&str> = line.split_whitespace().collect();
+        l1[idx] = numerals[0].parse::<u32>().unwrap();
+        l2[idx] = numerals[1].parse::<u32>().unwrap();
+        idx+=1;
+    }
+    l1.sort();
+    l2.sort();
+    let mut distance: u32 = 0;
+    let mut similarity: u32 = 0;
+    for index in 0..1000 {
+        distance += l1[index].abs_diff(l2[index]);
+        similarity += l1[index] * (l2.iter().filter(|&n| *n == l1[index]).count() as u32)
+    }
+    println!("Distance: {distance}");
+    println!("Similarity: {similarity}");
+}
