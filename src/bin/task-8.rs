209277ffd2858fded2db71ee8b6791477f5abfb4
shift-d <shiftkeydeveloper@gmail.com>
2024-12-13 16:15:25 +0300

    day 8 solved

--- src/bin/task-8.rs
+++ src/bin/task-8.rs
@@ -1,71 +1,130 @@
-use std::collections::HashMap;/*
-const INPUT: &str = "\
-............
-........0...
-.....0......
-.......0....
-....0.......
-......A.....
-............
-............
-........A...
-.........A..
-............
-............
-";*/
-const INPUT: &str = include_str!("../../input/8.txt");
-const BOUNDS: (isize, isize) = (49, 49);
+use std::fs::read_to_string;
+
+const INPUT_FILE: &str = "./input/8.txt";
+
+const EMPTY: char = '.';
+const ANTINODE: char = '#';
 
 #[derive(Clone)]
-struct Frequencies {
-    used: Vec<(isize, isize)>,
-    map: HashMap<char, Vec<(usize, usize)>>,
+struct Map {
+    data: Vec<Vec<char>>,
+    antinodes: Vec<Vec<char>>,
+    antennas: Vec<char>,
+    height: usize,
+    width: usize,
 }
 
-impl Frequencies {
-    fn init(input: &str) -> Self {
-        let mut used: Vec<(isize, isize)> = Vec::new();
-        let mut map: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
-        input.split('\n').enumerate().for_each(|(y, l)| {
-            l.chars().enumerate().for_each(|(x, c)| {
-                if c != '.' {
-                    used.push((x as isize, y as isize));
-                    match map.get_mut(&c) {
-                        Some(v) => v.push((x, y)),
-                        None => _ = map.insert(c, vec![(x, y)]),
-                    };
+impl Map {
+    fn from_data(data: Vec<Vec<char>>) -> Map {
+        let height = data.len();
+        let width = data[0].len();
+        let antinodes = vec![vec![EMPTY; width]; height];
+        let mut antennas: Vec<char> = Vec::new();
+        for c in data.iter().flatten().filter(|&x| *x != EMPTY) {
+            if !antennas.contains(c) {
+                antennas.push(*c)
+            }
+        }
+        Map {
+            data,
+            antinodes,
+            antennas,
+            height,
+            width,
+        }
+    }
+
+    fn compute_antinodes(&mut self) {
+        for antenna in &self.antennas {
+            let antennas = &self.get_antenna_positions(*antenna);
+            for i in 0..antennas.len() - 1 {
+                for j in i + 1..antennas.len() {
+                    let (ax, ay) = (antennas[i].0 as isize, antennas[i].1 as isize);
+                    let (bx, by) = (antennas[j].0 as isize, antennas[j].1 as isize);
+                    let (dx, dy) = (bx - ax, by - ay);
+                    let (an_x, an_y) = (ax - dx, ay - dy);
+                    if self.contains(an_x, an_y) {
+                        self.antinodes[an_y as usize][an_x as usize] = ANTINODE;
+                    }
+                    let (an_x, an_y) = (bx + dx, by + dy);
+                    if self.contains(an_x, an_y) {
+                        self.antinodes[an_y as usize][an_x as usize] = ANTINODE;
+                    }
                 }
-            });
-        });
-        Self { used, map }
+            }
+        }
     }
-    fn invalid(&mut self, node: (isize, isize)) -> bool {
-        let res = node.0 < 0 || node.1 < 0 || node.0 > BOUNDS.0 || node.1 > BOUNDS.1 || self.used.contains(&node);
-        self.used.push(node);
-        res
+
+    fn compute_antinodes_with_resonant_frequencies(&mut self) {
+        for antenna in &self.antennas {
+            let antennas = &self.get_antenna_positions(*antenna);
+            for i in 0..antennas.len() - 1 {
+                for j in i + 1..antennas.len() {
+                    let (ax, ay) = (antennas[i].0 as isize, antennas[i].1 as isize);
+                    let (bx, by) = (antennas[j].0 as isize, antennas[j].1 as isize);
+                    let (dx, dy) = (bx - ax, by - ay);
+
+                    let mut k = 0;
+                    while self.contains(ax - (k * dx), ay - (k * dy)) {
+                        let (an_x, an_y) = (ax - (k * dx), ay - (k * dy));
+                        self.antinodes[an_y as usize][an_x as usize] = ANTINODE;
+                        k += 1;
+                    }
+                    k = 0;
+                    while self.contains(bx + (k * dx), by + (k * dy)) {
+                        let (an_x, an_y) = (bx + (k * dx), by + (k * dy));
+                        self.antinodes[an_y as usize][an_x as usize] = ANTINODE;
+                        k += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    fn contains(&self, x: isize, y: isize) -> bool {
+        x >= 0 && x < self.width as isize && y >= 0 && y < self.height as isize
+    }
+
+    fn get_antenna_positions(&self, antenna: char) -> Vec<(usize, usize)> {
+        let mut positions: Vec<(usize, usize)> = Vec::new();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.data[y][x] == antenna {
+                    positions.push((x, y));
+                }
+            }
+        }
+        positions
+    }
+
+    fn count_antinodes(&self) -> usize {
+        self.antinodes
+            .iter()
+            .flatten()
+            .filter(|&x| *x == ANTINODE)
+            .count()
     }
 }
 
-fn anti_node(node_1: (usize, usize), node_2: (usize, usize)) -> (isize, isize) {
-    let node_1 = (node_1.0 as isize, node_1.1 as isize);
-    let node_2 = (node_2.0 as isize, node_2.1 as isize);
-    (
-        node_1.0 - node_2.0 + node_1.0,
-        node_1.1 - node_2.1 + node_1.1,
-    )
+fn part_one(mut map: Map) {
+    map.compute_antinodes();
+    println!("Total antinodes: {}", map.count_antinodes());
+}
+
+fn part_two(mut map: Map) {
+    map.compute_antinodes_with_resonant_frequencies();
+    println!("Total antinodes with resonance: {}", map.count_antinodes());
 }
 
 fn main() {
-    let freqs = Frequencies::init(INPUT);
-    let mut freqs_check = freqs.clone();
-    let nodes = freqs.map.values().map(|v| {
-        (0..v.len()).map(|a| {
-            (0..v.len()).filter(|b| {
-                if a != *b {
-                    !freqs_check.invalid(anti_node(v[a], v[*b]))
-                } else { false }
-            }).count()
-        }).sum::<usize>()
-    }).sum::<usize>();
-    println!("{nodes}");
+    let contents = read_to_string(INPUT_FILE).unwrap();
+    let map_data = contents
+        .trim()
+        .lines()
+        .map(|x| x.chars().collect())
+        .collect();
+    let map = Map::from_data(map_data);
+
+    part_one(map.clone());
+    part_two(map);
 }
