7bf21b3e1d22990bddc0c5cbe2861ad44a1b5c75
shift-d <shiftkeydeveloper@gmail.com>
2024-12-15 00:09:51 +0300

    day 11 pt. 2

--- src/bin/task-11.rs
+++ src/bin/task-11.rs
@@ -1,12 +1,13 @@
 const INPUT: &str = include_str!("../../input/11.txt");
 
 type Stones = Vec<u64>;
+use std::collections::HashMap;
 
 fn blink(stones: Stones) -> Stones {
     let mut blinked: Stones = stones;
     let mut buffer: Stones = Vec::new();
     blinked.iter_mut().for_each(|stone| {
-        if *stone == 0 { 
+        if *stone == 0 {
             *stone = 1;
         } else {
             let engraved = stone.to_string();
@@ -23,9 +24,33 @@ fn blink(stones: Stones) -> Stones {
     blinked
 }
 
+fn sum_blink_cached(cur: u64, to_iter: usize, cache: &mut HashMap<(u64, usize), u64>) -> usize {
+    if to_iter == 0 {
+        return 1;
+    }
+    if let Some(entry) = cache.get(&(cur, to_iter)) {
+        return *entry as usize;
+    }
+    let amount = blink(vec![cur])
+        .iter()
+        .map(|v| sum_blink_cached(*v, to_iter - 1, cache))
+        .sum::<usize>();
+    cache.insert((cur, to_iter), amount as u64);
+    amount
+}
+
 fn main() {
-    let stones: Stones = INPUT.replace('\n', "").split(' ').map(|n| n.parse::<u64>().unwrap()).collect::<_>();
-    println!("Before: {stones:?}");
+    let stones: Stones = INPUT
+        .replace('\n', "")
+        .split(' ')
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect::<_>();
     let blinked = (0..25).fold(stones.clone(), |acc, _| blink(acc));
-    println!("Amount: {}", blinked.len());
+    println!("After 25: {}", blinked.len());
+    let mut cache: HashMap<(u64, usize), u64> = HashMap::new();
+    let cached = stones
+        .iter()
+        .map(|s| sum_blink_cached(*s, 75, &mut cache))
+        .sum::<usize>();
+    println!("After 75: {}", cached);
 }
