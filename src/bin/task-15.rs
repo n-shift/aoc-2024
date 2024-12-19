3967204f5831dda3af6c99231fbd815b97f34aaa
shift-d <shiftkeydeveloper@gmail.com>
2024-12-19 21:27:40 +0300

    day 15 ref

--- src/bin/task-15.rs
+++ src/bin/task-15.rs
@@ -1,8 +1,7 @@
 const INPUT: &str = include_str!("../../input/15.txt");
 
-use std::cmp;
-use std::convert::From;
-#[derive(PartialEq)]
+use std::{cmp, convert::From};
+#[derive(PartialEq, Clone, Copy)]
 enum Cell {
     Robot,
     Box,
@@ -30,12 +29,6 @@ impl From<char> for Cell {
     }
 }
 
-impl Cell {
-    fn is_box(&self) -> bool {
-        self == &Cell::Box
-    }
-}
-
 impl From<char> for Dir {
     fn from(item: char) -> Self {
         match item {
@@ -49,9 +42,13 @@ impl From<char> for Dir {
 }
 
 struct Grid(Vec<Vec<Cell>>);
-impl From<Vec<Vec<Cell>>> for Grid {
-    fn from(item: Vec<Vec<Cell>>) -> Self {
-        Self(item)
+fn delta(before: (usize, usize), dir: Dir) -> (usize, usize) {
+    let (x, y) = before;
+    match dir {
+        Dir::Right => (x + 1, y),
+        Dir::Left => (x - 1, y),
+        Dir::Down => (x, y + 1),
+        Dir::Up => (x, y - 1),
     }
 }
 
@@ -64,39 +61,18 @@ impl Grid {
         let queue = queue.unwrap();
         let grid = &mut self.0;
         let (x, y) = queue.0;
-        let (lx, ly) = queue.1;
         grid[y][x] = Cell::Nothing;
-        match dir {
-            Dir::Up => {
-                grid[y - 1][x] = Cell::Robot;
-                if queue.0 != queue.1 {
-                    grid[ly - 1][x] = Cell::Box;
-                }
-            }
-            Dir::Right => {
-                grid[y][x + 1] = Cell::Robot;
-                if queue.0 != queue.1 {
-                    grid[y][lx + 1] = Cell::Box;
-                }
-            }
-            Dir::Down => {
-                grid[y + 1][x] = Cell::Robot;
-                if queue.0 != queue.1 {
-                    grid[ly + 1][x] = Cell::Box;
-                }
-            }
-            Dir::Left => {
-                grid[y][x - 1] = Cell::Robot;
-                if queue.0 != queue.1 {
-                    grid[y][lx - 1] = Cell::Box;
-                }
-            }
+        let (x, y) = delta((x, y), dir);
+        grid[y][x] = Cell::Robot;
+        if queue.0 != queue.1 {
+            let (lx, ly) = delta(queue.1, dir);
+            grid[ly][lx] = Cell::Box;
         }
     }
     fn queue(&self, dir: Dir) -> Option<((usize, usize), (usize, usize))> {
         let mut queue = ((0, 0), (0, 0));
-        let grid = &self.0;
-        let Some((y, Some(x))) = grid
+        let &Some((y, Some(x))) = &self
+            .0
             .iter()
             .map(|r| r.iter().position(|c| *c == Cell::Robot))
             .enumerate()
@@ -105,109 +81,61 @@ impl Grid {
             unreachable!()
         };
         queue.0 = (x, y);
-        match dir {
+        let c = |(f, c): &(usize, &Cell)| if **c == Cell::Box { Some(*f) } else { None };
+        let t = match dir {
+            Dir::Down | Dir::Up => &self.0.iter().map(|r| r[x]).collect(),
+            Dir::Right | Dir::Left => &self.0[y],
+        };
+        let mut q = t.iter().enumerate();
+        let q: Vec<(usize, &Cell)> = match dir {
             Dir::Down => {
-                let mut q = grid.iter().enumerate();
                 q.nth(y);
-                let wrap = q
-                    .map(|(fy, r)| if r[x].is_box() { Some(fy) } else { None })
-                    .take_while(Option::is_some)
-                    .flatten()
-                    .last();
-                queue.1 = if let Some(fy) = wrap { (x, cmp::max(fy, y)) } else { queue.0 };
+                q.collect()
             }
-            Dir::Up => {
-                let wrap = grid
-                    .iter()
-                    .enumerate()
-                    .take(y)
-                    .rev()
-                    .map(|(fy, r)| if r[x].is_box() { Some(fy) } else { None })
-                    .take_while(Option::is_some)
-                    .flatten()
-                    .last();
-                queue.1 = if let Some(fy) = wrap { (x, cmp::min(fy, y)) } else { queue.0 };
-            }
+            Dir::Up => q.take(y).rev().collect(),
             Dir::Right => {
-                let mut q = grid[y].iter().enumerate();
                 q.nth(x);
-                let wrap = q
-                    .map(|(fx, c)| if c.is_box() { Some(fx) } else { None })
-                    .take_while(Option::is_some)
-                    .flatten()
-                    .last();
-                queue.1 = if let Some(fx) = wrap { (cmp::max(fx, x), y) } else { queue.0 };
+                q.collect()
             }
-            Dir::Left => {
-                let wrap = grid[y]
-                    .iter()
-                    .enumerate()
-                    .take(x)
-                    .rev()
-                    .map(|(fx, c)| if c.is_box() { Some(fx) } else { None })
-                    .take_while(Option::is_some)
-                    .flatten()
-                    .last();
-                queue.1 = if let Some(fx) = wrap { (cmp::min(fx, x), y) } else { queue.0 };
-            }
-        }
-        let (fx, fy) = queue.1;
-        match dir {
-            Dir::Right => {
-                if self.0[fy][fx + 1] == Cell::Wall {
-                    None
-                } else {
-                    Some(queue)
-                }
-            }
-            Dir::Left => {
-                if self.0[fy][fx - 1] == Cell::Wall {
-                    None
-                } else {
-                    Some(queue)
-                }
-            }
-            Dir::Up => {
-                if self.0[fy - 1][fx] == Cell::Wall {
-                    None
-                } else {
-                    Some(queue)
-                }
-            }
-            Dir::Down => {
-                if self.0[fy + 1][fx] == Cell::Wall {
-                    None
-                } else {
-                    Some(queue)
-                }
-            }
+            Dir::Left => q.take(x).rev().collect(),
+        };
+        let wrap = q.iter().map(c).take_while(Option::is_some).flatten().last();
+        queue.1 = match dir {
+            Dir::Right => (cmp::max(x, wrap.unwrap_or(x)), y),
+            Dir::Left => (cmp::min(x, wrap.unwrap_or(x)), y),
+            Dir::Down => (x, cmp::max(y, wrap.unwrap_or(y))),
+            Dir::Up => (x, cmp::min(y, wrap.unwrap_or(y))),
+        };
+
+        let (fx, fy) = delta(queue.1, dir);
+        if self.0[fy][fx] == Cell::Wall {
+            None
+        } else {
+            Some(queue)
         }
     }
     fn gps(&self) -> usize {
         self.0
             .iter()
             .map(|r| {
-                let t = r
-                    .iter()
+                r.iter()
                     .enumerate()
-                    .filter(|(_x, c)| c.is_box())
-                    .map(|(x, _c)| x)
-                    .collect::<Vec<_>>();
-                (t.iter().sum::<usize>(), t.len())
+                    .filter(|(_, c)| **c == Cell::Box)
+                    .fold((0, 0), |acc, (x, _)| (acc.0 + x, acc.1 + 1))
             })
             .enumerate()
-            .map(|(y, (x, n))| 100 * y * n + x)
-            .sum::<usize>()
+            .fold(0, |acc, (y, (x, n))| acc + 100 * y * n + x)
     }
 }
 
 fn main() {
     let [raw_grid, raw_inst] = INPUT.split("\n\n").collect::<Vec<_>>()[..] else { unreachable!() };
-    let mut grid: Grid = raw_grid
-        .split('\n')
-        .map(|l| l.chars().map(Cell::from).collect::<Vec<Cell>>())
-        .collect::<Vec<_>>()
-        .into();
-    raw_inst.chars().filter(|c| *c != '\n').map(Dir::from).for_each(|d| grid.shift(d));
+    let mut grid = Grid(
+        raw_grid
+            .lines()
+            .map(|l| l.chars().map(Cell::from).collect::<Vec<Cell>>())
+            .collect::<Vec<_>>(),
+    );
+    raw_inst.chars().filter(|c| *c != '\n').for_each(|c| grid.shift(c.into()));
     println!("GPS sum: {}", grid.gps());
 }
